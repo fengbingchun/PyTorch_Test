Fix batch normalization impl

demo/pytorch/batch_normalization/test_batch_normalization.py
@@ -4,96 +4,68 @@ import numpy as np
 
 # Blog: https://blog.csdn.net/fengbingchun/article/details/118959997
 
-# reference: https://github.com/Johann-Huber/batchnorm_pytorch/blob/main/batch_normalization_in_pytorch.ipynb
-
+# reference: https://zh.d2l.ai/chapter_convolutional-modern/batch-norm.html
 # BatchNorm reimplementation
-class myBatchNorm2d(nn.Module):
-    def __init__(self, input_size = None , epsilon = 1e-5, momentum = 0.99):
-        super(myBatchNorm2d, self).__init__()
-        assert input_size, print('Missing input_size parameter.')
-
-        # Batch mean & var must be defined during training
-        self.mu = torch.zeros(1, input_size)
-        self.var = torch.ones(1, input_size)
-
-        # For numerical stability
-        self.epsilon = epsilon
-
-        # Exponential moving average for mu & var update
-        self.it_call = 0  # training iterations
-        self.momentum = momentum # EMA smoothing
-
-        # Trainable parameters
-        self.beta = torch.nn.Parameter(torch.zeros(1, input_size))
-        self.gamma = torch.nn.Parameter(torch.ones(1, input_size))
-
-        # Batch size on which the normalization is computed
-        self.batch_size = 0
-
-    def forward(self, x):
-        # [batch_size, input_size]
-
-        self.it_call += 1
-
-        if self.training:
-            print("Info: training ...")
-            if( self.batch_size == 0 ):
-                # First iteration : save batch_size
-                self.batch_size = x.shape[0]
-
-            # Training : compute BN pass
-            #batch_mu = (x.sum(dim=0)/x.shape[0]).unsqueeze(0) # [1, input_size]
-            batch_mu = torch.mean(x, dim=0)
-            #batch_var = (x.var(dim=0)/x.shape[0]).unsqueeze(0)*2 # [1, input_size]
-            batch_var = torch.var(x, unbiased=False, dim=0)
-            #print("batch_mu:", batch_mu)
-            #print("batch_var:", batch_var)
-
-            x_normalized = (x-batch_mu)/torch.sqrt(batch_var + self.epsilon) # [batch_size, input_size]
-            x_bn = self.gamma * x_normalized + self.beta # [batch_size, input_size]
-
-            # Update mu & std
-            if(x.shape[0] == self.batch_size):
-                running_mu = batch_mu
-                running_var = batch_var
-            else:
-                running_mu = batch_mu*self.batch_size/x.shape[0]
-                running_var = batch_var*self.batch_size/x.shape[0]
-
-            self.mu = running_mu * (self.momentum/self.it_call) + \
-                            self.mu * (1 - (self.momentum/self.it_call))
-            self.var = running_var * (self.momentum/self.it_call) + \
-                        self.var * (1 - (self.momentum/self.it_call))
-
+def batch_norm(X, gamma, beta, moving_mean, moving_var, eps, momentum):
+    # 通过is_grad_enabled来判断当前模式是训练模式还是预测模式
+    if not torch.is_grad_enabled():
+        # 如果是在预测模式下，直接使用传入的移动平均所得的均值和方差
+        X_hat = (X - moving_mean) / torch.sqrt(moving_var + eps)
+    else:
+        assert len(X.shape) in (2, 4)
+        if len(X.shape) == 2:
+            # 使用全连接层的情况，计算特征维上的均值和方差
+            mean = X.mean(dim=0)
+            var = ((X - mean) ** 2).mean(dim=0)
         else:
-            print("Info: inference ...")
-            # Inference: compute BN pass using estimated mu & var
-            if (x.shape[0] == self.batch_size):
-                estimated_mu = self.mu
-                estimated_var = self.var
-            else :
-                estimated_mu = self.mu*x.shape[0]/self.batch_size
-                estimated_var = self.var*x.shape[0]/self.batch_size
+            # 使用二维卷积层的情况，计算通道维上（axis=1）的均值和方差。
+            # 这里我们需要保持X的形状以便后面可以做广播运算
+            mean = X.mean(dim=(0, 2, 3), keepdim=True)
+            var = ((X - mean) ** 2).mean(dim=(0, 2, 3), keepdim=True)
+        # 训练模式下，用当前的均值和方差做标准化
+        X_hat = (X - mean) / torch.sqrt(var + eps)
+        # 更新移动平均的均值和方差
+        moving_mean = momentum * moving_mean + (1.0 - momentum) * mean
+        moving_var = momentum * moving_var + (1.0 - momentum) * var
+    Y = gamma * X_hat + beta  # 缩放和移位
+    return Y, moving_mean.data, moving_var.data
 
-            x_normalized = (x-estimated_mu)/torch.sqrt(estimated_var + self.epsilon) # [batch_size, input_size]
-            x_bn = self.gamma * x_normalized + self.beta # [batch_size, input_size]
+class BatchNorm(nn.Module):
+    # num_features：完全连接层的输出数量或卷积层的输出通道数。
+    # num_dims：2表示完全连接层，4表示卷积层
+    def __init__(self, num_features, num_dims):
+        super().__init__()
+        if num_dims == 2:
+            shape = (1, num_features)
+        else:
+            shape = (1, num_features, 1, 1)
+        # 参与求梯度和迭代的拉伸和偏移参数，分别初始化成1和0
+        self.gamma = nn.Parameter(torch.ones(shape))
+        self.beta = nn.Parameter(torch.zeros(shape))
+        # 非模型参数的变量初始化为0和1
+        self.moving_mean = torch.zeros(shape)
+        self.moving_var = torch.ones(shape)
 
-        return x_bn # [batch_size, output_size=input_size]
+    def forward(self, X):
+        # 如果X不在内存上，将moving_mean和moving_var复制到X所在显存上
+        if self.moving_mean.device != X.device:
+            self.moving_mean = self.moving_mean.to(X.device)
+            self.moving_var = self.moving_var.to(X.device)
+        # 保存更新过的moving_mean和moving_var
+        Y, self.moving_mean, self.moving_var = batch_norm(
+            X, self.gamma, self.beta, self.moving_mean,
+            self.moving_var, eps=1e-5, momentum=0.9)
+        return Y
 
 # N = 3, C = 1, H = 1, W = 6
-input_size = 1 # channel
-bn = myBatchNorm2d(input_size)
 data =  [[[[11.1, -2.2, 23.3, 54.4, 58.5, -16.6]]],
 		[[[-97.7, -28.8, 49.9, -61.3, 52.6, -33.9]]],
 		[[[-2.45, -15.7, 72.4, 9.1, 47.2, 21.7]]]]
 input = torch.FloatTensor(data) # [N, C, H, W]
-print("input:", input)
-output = bn.forward(input)
+print("input shape:", input.shape)
+
+model = BatchNorm(1, 2)
+output = model(input)
 print("output:", output)
 
-'''
-print("######################")
-a = np.array(data)
-print(np.mean(a, axis=0))
-print(np.var(a, axis=0))
-'''
+print("test finish")